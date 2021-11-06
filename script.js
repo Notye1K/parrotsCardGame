ef70e705e9cdb54c7ffda3953ab00ae245b07d21
Kl3yton <kleyton.vcc@gmail.com>
2021-11-06 15:49:23 -0300

    fim do jogo

--- script.js
+++ script.js
@@ -36,20 +36,39 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 function clic(x) {
-    i++
-    if (i == 2){
-        i=0
-        if (first == x.lastElementChild.innerHTML) {
-            alert('acertou')
+    if (x.classList.contains('matched') == false) {
+        times++
+        let end = false
+        const QueryParrot = document.querySelectorAll('.parrot')
+        i++
+        if (i == 2){
+            i=0
+            if (firstInner == x.lastElementChild.innerHTML) {
+                alert('acertou')
+                first.parentElement.classList.add('matched')
+                x.classList.add('matched')
+                for (let c = 0; c < QueryParrot.length; c++) {
+                    if (QueryParrot[c].classList.contains('matched')){
+                        end = true
+                    }
+                    else{
+                        end = false
+                        break
+                    }
+                }
+                if (end) {
+                    alert(`Você ganhou em ${times} jogadas!`)                
+                }
+            }
+            else {
+                alert('errou')
+            }
         }
-        else {
-            alert('errou')
-        }
+        firstInner = x.lastElementChild.innerHTML
+        first = x.lastElementChild
+        //console.log(x.firstElementChild)
+        //console.log(x.lastElementChild)
     }
-    first = x.lastElementChild.innerHTML
-    console.log(first)
-    //console.log(x.firstElementChild)
-    //console.log(x.lastElementChild)
 }
 
 
@@ -57,4 +76,6 @@ function clic(x) {
 let images = ['img src="images/bobrossparrot.gif" alt="bobross"','img src="images/bobrossparrot.gif" alt="bobross"','img src="images/explodyparrot.gif" alt="explody"','img src="images/explodyparrot.gif" alt="explody"','img src="images/fiestaparrot.gif" alt="fiesta"','img src="images/fiestaparrot.gif" alt="fiesta"','img src="images/metalparrot.gif" alt="metal"','img src="images/metalparrot.gif" alt="metal"','img src="images/revertitparrot.gif" alt="reverti"','img src="images/revertitparrot.gif" alt="reverti"','img src="images/tripletsparrot.gif" alt="triplets"','img src="images/tripletsparrot.gif" alt="triplets"','img src="images/unicornparrot.gif" alt="unicorn.gif"','img src="images/unicornparrot.gif" alt="unicorn.gif"']
 numberCards()
 let i = 0
+let firstInner
 let first
+let times = 0
