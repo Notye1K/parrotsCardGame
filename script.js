9908b48b7c0991f4d952368f548e673254a86c1e
Kl3yton <kleyton.vcc@gmail.com>
2021-11-06 14:58:57 -0300

    match das cartas

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function numberCards() {
     let cards = 0
+    checkiandj:
     while (cards < 4 || cards > 14) {
-        cards = parseInt(prompt("Coloque o número de cartas que quer jogar (entre 4 e 14)"))
+        cards = parseInt(prompt("Coloque o número de cartas que quer jogar (entre 4 e 14, número par)"))
         let resto = cards % 2
         if (resto != 0) {
-            cards++
+            cards = 0
         }
     }
 
@@ -19,11 +20,11 @@ function numberCards() {
     inner.innerHTML = ""
     while (c !== cards) {
         inner.innerHTML = inner.innerHTML + `        
-        <div class="parrot">
-            <div class ="front-face face">
+        <div class="parrot" data-identifier="card" onclick="clic(this)">
+            <div class ="front-face face" data-identifier="front-face">
                 <img src="images/front.png" alt="front">
             </div>
-            <div class ="back-face face">       
+            <div class ="back-face face" data-identifier="back-face">       
                 <${cardsSorted[c]}>
             </div>
         </div>
@@ -34,8 +35,26 @@ function numberCards() {
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+function clic(x) {
+    i++
+    if (i == 2){
+        i=0
+        if (first == x.lastElementChild.innerHTML) {
+            alert('acertou')
+        }
+        else {
+            alert('errou')
+        }
+    }
+    first = x.lastElementChild.innerHTML
+    console.log(first)
+    //console.log(x.firstElementChild)
+    //console.log(x.lastElementChild)
+}
 
 
 
 let images = ['img src="images/bobrossparrot.gif" alt="bobross"','img src="images/bobrossparrot.gif" alt="bobross"','img src="images/explodyparrot.gif" alt="explody"','img src="images/explodyparrot.gif" alt="explody"','img src="images/fiestaparrot.gif" alt="fiesta"','img src="images/fiestaparrot.gif" alt="fiesta"','img src="images/metalparrot.gif" alt="metal"','img src="images/metalparrot.gif" alt="metal"','img src="images/revertitparrot.gif" alt="reverti"','img src="images/revertitparrot.gif" alt="reverti"','img src="images/tripletsparrot.gif" alt="triplets"','img src="images/tripletsparrot.gif" alt="triplets"','img src="images/unicornparrot.gif" alt="unicorn.gif"','img src="images/unicornparrot.gif" alt="unicorn.gif"']
 numberCards()
+let i = 0
+let first
